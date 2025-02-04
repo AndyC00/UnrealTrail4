e40acfc31ac0dcf6b7f71068131f816cfbd24208
AndyC00 <chenjinzhi96@gmail.com>
2025-02-04 16:23:08 +1300

    init

---
+++ Source/Trail4/Trail4.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Trail4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Trail4, "Trail4" );
